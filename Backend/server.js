56f33f0b5a64e9d68d9429ccc7660588f4fbc912
AdityaPourush <adityapourush11@outlook.com>
2024-02-11 21:10:27 +0530

    cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const mongoose = require('mongoose');
 const {connectToDB, isConnected} = require('./db.js');
 const cors = require("cors");
-app.use("cors")
+app.use(cors())
 
 //Connect to MongoDB
 connectToDB();
