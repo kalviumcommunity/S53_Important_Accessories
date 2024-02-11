87e3cca17fff74fb543497b41cd5bff942380c73
AdityaPourush <adityapourush11@outlook.com>
2024-02-11 19:51:59 +0530

    card and search bar replaced

--- Frontend/accessory_list_app/src/App.jsx
+++ Frontend/accessory_list_app/src/App.jsx
@@ -6,6 +6,11 @@ import CardsContainer from './Components/CardsContainer'
 import { Route, Routes } from 'react-router-dom'
 import Cards from './Components/Cards'
 import ItemsForm from './Components/ItemsForm'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
