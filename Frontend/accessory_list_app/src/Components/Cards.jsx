e4d593d355eeb7ddc5ceb7bd5271c1a840dd4520
AdityaPourush <adityapourush11@outlook.com>
2024-03-02 17:46:33 +0530

    users fetching

--- Frontend/accessory_list_app/src/Components/Cards.jsx
+++ Frontend/accessory_list_app/src/Components/Cards.jsx
@@ -9,35 +9,21 @@ import { Button } from "../assets/Shadcn UI Components/button";
 import { Link } from "react-router-dom";
 
 const Cards = () => {
-  const [items, setItems] = useState([]);
+  const [items, SetItems] = useState([]);
   const [modalcategory, setModalCategory] = useState("");
   const [deleteId, setDeleteId] = useState("");
-  const [users, setUsers] = useState([]);
-  const [selectValue, setSelectValue] = useState("");
 
-  const showModal = (category, id) => {
-    document.getElementById("my_modal_1").showModal();
-    setModalCategory(category);
-    setDeleteId(id);
-  };
+  const showModal = (category, id)=> {
+    document.getElementById("my_modal_1").showModal()
+    setModalCategory(category)
+    setDeleteId(id)
+  }
 
   const fetchData = () => {
     axios
       .get("https://s53-important-accessories.onrender.com/accessory")
       .then((res) => {
-        setItems(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const fetchUsers = () => {
-    axios
-      .get("https://s53-important-accessories.onrender.com/user")
-      .then((res) => {
-        setUsers(res.data);
+        SetItems(res.data);
         console.log(res.data);
       })
       .catch((err) => {
@@ -47,27 +33,18 @@ const Cards = () => {
 
   useEffect(() => {
     fetchData();
-    fetchUsers();
   }, []);
 
   const handleDelete = () => {
-    axios
-      .delete(
-        `https://s53-important-accessories.onrender.com/accessory/${deleteId}`
-      )
-      .then(() => {
-        fetchData();
-        console.log({ modalcategory, deleteId }, "deleted from database");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  // Filter items based on selected user ID
-  const filteredItems = selectValue
-    ? items.filter((item) => item.user_id === selectValue)
-    : items;
+    axios.delete(`https://s53-important-accessories.onrender.com/accessory/${deleteId}`)
+    .then(()=>{
+      fetchData();
+      console.log({modalcategory, deleteId}, "deleted from database");
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
 
   return (
     <>
@@ -76,44 +53,33 @@ const Cards = () => {
         type={"search"}
         placeholder="🔍  Search Items"
       />
-      <select
-        value={selectValue}
-        onChange={(e) => setSelectValue(e.target.value)}
-      >
-        <option value="">All Users</option>
-        {users.map((option) => (
-          <option value={option._id} key={option._id}>
-            {option.name}
-          </option>
-        ))}
-      </select>
       <div className="grid grid-cols-1 md:grid-cols-2 m-[auto] gap-[3vmin] bg-amber-100 text-center p-5">
-        {filteredItems.map((item) => {
+        {items.map((items) => {
           return (
             <Card
               className="max-w-sm m-[auto] glass bg-yellow-200 min-h-[45vh]"
-              key={item._id}
-              imgSrc={item.image}
+              key={items._id}
+              imgSrc={items.image}
               horizontal
             >
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {item.category}
+                {items.category}
               </h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">
-                {item.description}
+                {items.description}
               </p>
               <p className="font-normal text-gray-800 dark:text-gray-600">
-                Average Buy: {item.averageBuyPrice}
+                Average Buy: {items.averageBuyPrice}
               </p>
               <a
-                href={item.amazonLink}
+                href={items.amazonLink}
                 target="blank"
                 className="btn btn-primary"
               >
                 Buy Now
               </a>
               <div className="flex justify-between">
-                <Link to={`/update/${item._id}`}>
+                <Link to={`/update/${items._id}`}>
                   <Button
                     variant="secondary"
                     className="bg-slate-400 hover:bg-slate-500"
@@ -124,7 +90,7 @@ const Cards = () => {
                 <Button
                   variant="destructive"
                   className="bg-red-400 hover:bg-red-500"
-                  onClick={() => showModal(item.category, item._id)}
+                  onClick={() => showModal(items.category, items._id)}
                 >
                   Delete
                 </Button>
@@ -132,24 +98,12 @@ const Cards = () => {
                 <dialog id="my_modal_1" className="modal">
                   <div className="modal-box">
                     <h3 className="font-bold text-lg">Are You Sure !</h3>
-                    <p className="py-4">
-                      You want to Delete {modalcategory} ?
-                    </p>
+                    <p className="py-4">You want to Delete {modalcategory} ?</p>
                     <div className="modal-action">
-                      <form
-                        method="dialog"
-                        className="flex justify-evenly w-[100%]"
-                      >
+                      <form method="dialog" className="flex justify-evenly w-[100%]">
                         {/* if there is a button in form, it will close the modal */}
-                        <button className="btn bg-blue-400 hover:bg-blue-500">
-                          Cancel
-                        </button>
-                        <button
-                          className="btn bg-red-400 hover:bg-red-500"
-                          onClick={() => handleDelete()}
-                        >
-                          Delete
-                        </button>
+                        <button className="btn bg-blue-400 hover:bg-blue-500">Cancel</button>
+                        <button className="btn bg-red-400 hover:bg-red-500" onClick={()=>handleDelete()}>Delete</button>
                       </form>
                     </div>
                   </div>
